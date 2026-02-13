d2659c2d7fe9616b22f9bba1198fc0efb0aac2c4
giftedly <52332868+giftedl@users.noreply.github.com>
2026-02-13 17:51:38 -0600

    Add lesdar command to check user's lesbian meter

---
+++ src/commands/fun/lesdar.js
@@ -0,0 +1,90 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const commandLogging = require("../../config/logging/commandlog");
+const darlogging = require("../../config/logging/darlog");
+const DarList = require("../../../mongo/models/idDarSchema");
+
+const utility_functions = {
+  chance: function (probability) {
+    return Math.random() <= probability;
+  },
+  number_format_commas: function (number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("lesdar")
+    .setDescription("How lesbian are you?")
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("See how lesbian a user is")
+        .setRequired(false)
+    ),
+
+  async execute(interaction, client) {
+    await interaction.deferReply(); 
+
+    const targetUser =
+      interaction.options.getUser("target") || interaction.user;
+    const userName = targetUser.username;
+    const userid = targetUser.id;
+
+    let meter;
+    try {
+      const darList = await DarList.findOne();
+
+      if (darList) {
+        const lesdarEntry = darList.lesdar.find(
+          (entry) => entry.userid === userid
+        );
+
+        if (lesdarEntry) {
+          meter = lesdarEntry.meter;
+        } else {
+          meter = Math.floor(Math.random() * 101);
+          if (utility_functions.chance(0.0001)) {
+            meter = Math.floor(Math.random() * 2354082) + 500;
+            if (utility_functions.chance(0.5)) {
+              meter *= -1;
+            }
+          }
+        }
+      } else {
+        meter = Math.floor(Math.random() * 101);
+
+        if (utility_functions.chance(0.0001)) {
+          meter = Math.floor(Math.random() * 2354082) + 500;
+          if (utility_functions.chance(0.5)) {
+            meter *= -1;
+          }
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      meter = Math.floor(Math.random() * 101); 
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(`How lesbian is ${userName}?`)
+      .setDescription(
+        `<@${userid}> is **${utility_functions.number_format_commas(
+          meter
+        )}% lesbian!**`
+      )
+      .setColor(0xff00ae)
+      .setFooter({
+        text: "The bot has 99.99% accuracy rate on checking users lesbianness",
+      });
+
+    try {
+      await interaction.editReply({ embeds: [embed] }); // Edit the deferred reply
+    } catch (error) {
+      console.error("Error sending response:", error);
+    }
+
+    await commandLogging(client, interaction);
+    await darlogging(client, "Lesdar", userName, meter, userid);
+  },
+};
