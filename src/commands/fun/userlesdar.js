1cfd40992f27516915904885cfa204be08748a00
giftedly <52332868+giftedl@users.noreply.github.com>
2026-02-13 17:52:31 -0600

    Add User Lesdar context menu command

---
+++ src/commands/fun/userlesdar.js
@@ -0,0 +1,77 @@
+const {
+  EmbedBuilder,
+  ContextMenuCommandBuilder,
+  ApplicationCommandType,
+} = require("discord.js");
+const DarList = require("../../../mongo/models/idDarSchema");
+const darlogging = require("../../config/logging/darlog");
+
+const utility_functions = {
+  chance: function (probability) {
+    return Math.random() <= probability;
+  },
+  number_format_commas: function (number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
+};
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName("User Lesdar")
+    .setType(ApplicationCommandType.User),
+
+  async execute(interaction, client) {
+    const targetUser = interaction.targetUser;
+    const userName = targetUser.username;
+    const userid = targetUser.id;
+
+    let meter;
+    try {
+      const darList = await DarList.findOne();
+
+      if (darList) {
+        const gaydarEntry = darList.lesdar.find(
+          (entry) => entry.userid === userid
+        );
+
+        if (gaydarEntry) {
+          meter = gaydarEntry.meter;
+        } else {
+          meter = Math.floor(Math.random() * 101);
+          if (utility_functions.chance(0.0001)) {
+            meter = Math.floor(Math.random() * 2354082) + 500;
+            if (utility_functions.chance(0.5)) {
+              meter *= -1;
+            }
+          }
+        }
+      } else {
+        meter = Math.floor(Math.random() * 101);
+        if (utility_functions.chance(0.0001)) {
+          meter = Math.floor(Math.random() * 2354082) + 500;
+          if (utility_functions.chance(0.5)) {
+            meter *= -1;
+          }
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      meter = Math.floor(Math.random() * 101);
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(`How lesbian is ${userName}?`)
+      .setDescription(
+        `<@${userid}> is **${utility_functions.number_format_commas(
+          meter
+        )}% lesbian!**`
+      )
+      .setColor(0xff00ae)
+      .setFooter({
+        text: "The bot has 99.99% accuracy rate on checking users lesbianness",
+      });
+
+    await interaction.reply({ embeds: [embed] });
+    await darlogging(client, "User Gaydar", userName, meter, userid);
+  },
+};
